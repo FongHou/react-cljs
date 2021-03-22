83170084fcfaa9262fed315bd5255ba50fd4d9c9
FongHou <you@example.com>
2021-03-22 01:23:21 -0400

    update

--- src/app/store.cljs
+++ src/app/store.cljs
@@ -1,23 +1,18 @@
 (ns app.store
-  (:require
-   [clojure.spec.alpha :as spec]
-   [cljsjs.use-store :refer [use-atom derived-atom]]
-   [okulary.core :as ol]))
+  (:require [cljsjs.use-store :refer [use-atom derived-atom]]))
 
 (def init-state {:count 0})
 
 (defmulti reducer (fn [_state [event]] event))
 
-(let [[atom provider use-dispatch]
-      (use-atom reducer init-state)]
+(let [[atom provider use-dispatch] (use-atom reducer init-state)]
   (def store-atom atom)
   (def store-provider provider)
   (def use-dispatch use-dispatch))
 
-
 (comment
 
-  (require '[flow-storm.api :as fsa]) 
+  (require '[flow-storm.api :as fsa])
 
   (fsa/trace-ref store-atom)
 
