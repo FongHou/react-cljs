8bd14326ebb318d661239575cfaeada6f59d23f0
FongHou <you@example.com>
2021-04-14 21:02:16 -0400

    update

--- src/app/preload.cljs
+++ src/app/preload.cljs
@@ -1,4 +1,9 @@
 (ns app.preload
-  (:require [flow-storm.api :as fsa]))
+  (:require [flow-storm.api :as fsa]
+            [app.store :as app]))
+
+(set! *warn-on-infer* true)
 
 (fsa/connect)
+
+(fsa/trace-ref app/store-atom)
